ee75eade4ce7103fe62287f6c2043da525e37701
OliveZOE <154832729+OliveZOE@users.noreply.github.com>
2023-12-26 11:00:34 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="zaaaaaa" &&  pwd=="zoe19941226$"){
+    if(userName=="zaaaaa" &&  pwd=="zoe1226$"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
